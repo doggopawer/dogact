2b5f4719b2b85664c2f1b35fa1134e67c8ef4d0e
doggopawer <doggopawer@gmail.com>
2022-06-13 17:50:26 +0900

    add createDOM, render, createElement, makeElement\

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # dogact
-dogact is ui libary cloning react.
+dogact is UI libary cloning react for **study**.
+
+## usage
+
+```
+import { createDOM, render, makeElement } from "./dogact.js";
+
+const h1 = makeElement('h1');
+const div = makeElement('div');
+const ul = makeElement('ul');
+const li = makeElement('li');
+
+const List = () => {
+    return (
+        ul({},
+            li({style: "color:red"}, "1st item"),
+            li({style: "color:blue"}, "2nd item"),
+            li({style: "color:green"}, "3rd item"),
+        )
+    )
+}
+const App = () => {
+    return (
+        div( {},
+            h1( {}, "Dogact start"),
+            List()
+        )
+    );
+};
+
+
+render('#root', createDOM(App()));
+```
--- dogact.js
+++ dogact.js
@@ -1,1 +1,30 @@
-test1
+export const createDOM = (node) => {
+    if (typeof node === 'string') {
+        return document.createTextNode(node);
+    }
+
+    const element = document.createElement(node.tag);
+
+    Object.entries(node.props)
+        .forEach(([name, value]) => element.setAttribute(name, value));
+
+    node.children
+        .map(createDOM)
+        .forEach(element.appendChild.bind(element))
+    return element;
+}
+export const render = (root, container) => {
+    return document
+        .querySelector(root)
+        .appendChild(container);
+}
+export const createElement = (tag, props, ...children) => {
+    return { tag, props, children }
+}
+export const makeElement = (tag) => (props, ...children) => createElement(tag, props, ...children);
+
+
+
+
+
+
